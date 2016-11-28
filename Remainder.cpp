decec1b21044873ab9234db4d843a4ab50106239
Hammedi <abrahamedina24@gmail.com>
2016-11-27 18:19:06 -0800

    Commented Code

--- Remainder.cpp
+++ Remainder.cpp
@@ -1,17 +1,22 @@
+// Author: Abraham Medina
+// Last Modified: 11/27/2016
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	// Initializing variables
 	int smallestNum = 1;
 	bool pass = false;
 	
-	while(pass == false)
+	while(pass == false) // Loops while smallestNum is not divisible by 1-20
 	{
-		pass = true;
-		for(int i = 1; i <= 20; i++)
+		pass = true; // Sets pass to true before smallestNum is tested
+		for(int i = 1; i <= 20; i++) // Cycles denominator from 1-20
 		{
-			if(smallestNum % i != 0)
+			// smallestNum set to false if NOT evenly divisible by 1-20
+			if(smallestNum % i != 0) 
 			{
 				pass = false;
 				smallestNum++;
